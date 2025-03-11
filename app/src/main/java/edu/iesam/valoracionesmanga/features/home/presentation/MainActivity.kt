f0d568c91b025ffcd7d06820d2ed06476b9d40a2
Javier Lozano <117869190+JavierLozHer@users.noreply.github.com>
2025-03-11 16:37:19 +0100

    Commit inicial

---
+++ app/src/main/java/edu/iesam/valoracionesmanga/features/home/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package edu.iesam.valoracionesmanga.features.home.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.iesam.valoracionesmanga.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
