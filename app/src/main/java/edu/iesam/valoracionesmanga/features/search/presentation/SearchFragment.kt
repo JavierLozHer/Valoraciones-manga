dec8f2a0323c83bbcb51daa9181d47db6d7cdab8
Javier Lozano <117869190+JavierLozHer@users.noreply.github.com>
2025-03-23 12:29:11 +0100

    Crear menu principal

---
+++ app/src/main/java/edu/iesam/valoracionesmanga/features/search/presentation/SearchFragment.kt
@@ -0,0 +1,28 @@
+package edu.iesam.valoracionesmanga.features.search.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import edu.iesam.valoracionesmanga.R
+import edu.iesam.valoracionesmanga.databinding.FragmentSearchBinding
+
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.searchToolbar.toolbar.title = getString(R.string.search)
+    }
+}
