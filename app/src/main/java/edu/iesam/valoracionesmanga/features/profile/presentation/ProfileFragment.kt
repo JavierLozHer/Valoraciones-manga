d71048508327eb60e438f72f6a4548d63df33439
Javier Lozano <117869190+JavierLozHer@users.noreply.github.com>
2025-04-14 09:44:44 +0200

    Mostrar valoraciones usuario registrado

--- app/src/main/java/edu/iesam/valoracionesmanga/features/profile/presentation/ProfileFragment.kt
+++ app/src/main/java/edu/iesam/valoracionesmanga/features/profile/presentation/ProfileFragment.kt
@@ -6,15 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.faltenreich.skeletonlayout.Skeleton
+import com.faltenreich.skeletonlayout.applySkeleton
 import edu.iesam.valoracionesmanga.R
+import edu.iesam.valoracionesmanga.core.domain.ErrorApp
+import edu.iesam.valoracionesmanga.core.presentation.assessmentAdapter.AssessmentAdapter
+import edu.iesam.valoracionesmanga.core.presentation.errorView.ErrorAppUIFactory
+import edu.iesam.valoracionesmanga.core.presentation.hide
 import edu.iesam.valoracionesmanga.databinding.FragmentProfileBinding
+import edu.iesam.valoracionesmanga.features.assessment.domain.AssessmentManga
+import edu.iesam.valoracionesmanga.features.profile.domain.User
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
+    private val assessmentAdapter = AssessmentAdapter(null)
+
     private val viewModel : ProfileViewModel by viewModel()
+    private val errorAppUIFactory: ErrorAppUIFactory by inject()
+
+    private lateinit var skeleton : Skeleton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,20 +42,51 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.profileToolbar.toolbar.title = getString(R.string.profile)
+        setupRecyclerView()
         setUpObserver()
         viewModel.getUserLogged()
     }
 
+    private fun setupRecyclerView() {
+        binding.apply {
+            assessmentRecyclerView.layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+            assessmentRecyclerView.adapter = assessmentAdapter
+            skeleton = assessmentRecyclerView.applySkeleton(R.layout.view_assessment_item)
+        }
+
+    }
+
     private fun setUpObserver() {
         viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
+            showLoading(uiState.isLoading)
+            bindData(uiState.assessment, uiState.user)
+            showError(uiState.errorApp)
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            skeleton.showSkeleton()
+        } else {
+            skeleton.showOriginal()
+        }
+    }
+
+    private fun bindData(assessments: List<AssessmentManga>?, user: User?) {
+        assessments?.let {
+            assessmentAdapter.submitList(assessments)
+            binding.buttonsLogin.visibility = View.GONE
+        } ?: run {
             binding.apply {
-                uiState.user?.let {
-                    user.visibility = View.VISIBLE
-                    buttonsLogin.visibility = View.GONE
-                    userName.text = it.email
+                user?.let {
+                    viewModel.getAssessment(user.email)
+                    binding.buttonsLogin.visibility = View.GONE
                 }?: run {
                     buttonsLogin.visibility = View.VISIBLE
-                    user.visibility = View.GONE
                     buttonLogin.setOnClickListener {
                         findNavController().navigate(ProfileFragmentDirections.actionProfileToUserForm(true))
                     }
@@ -52,7 +98,14 @@ class ProfileFragment : Fragment() {
         }
     }
 
-
-
+    private fun showError(errorApp: ErrorApp?) {
+        errorApp?.let {
+            viewModel.getUserLogged()
+            val errorAppUI = errorAppUIFactory.build(errorApp, viewModel::getUserLogged)
+            binding.errorAppView.render(errorAppUI)
+        } ?: run {
+            binding.errorAppView.hide()
+        }
+    }
 
 }
--- app/src/main/java/edu/iesam/valoracionesmanga/features/profile/presentation/ProfileViewModel.kt
+++ app/src/main/java/edu/iesam/valoracionesmanga/features/profile/presentation/ProfileViewModel.kt
@@ -4,6 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import edu.iesam.valoracionesmanga.core.domain.ErrorApp
+import edu.iesam.valoracionesmanga.features.assessment.domain.AssessmentManga
+import edu.iesam.valoracionesmanga.features.assessment.domain.GetAssessmentByUserEmailUseCase
+import edu.iesam.valoracionesmanga.features.assessment.domain.GetAssessmentMangaUseCase
+import edu.iesam.valoracionesmanga.features.manga.domain.GetMangasUseCase
 import edu.iesam.valoracionesmanga.features.profile.domain.GetUserLoggedUseCase
 import edu.iesam.valoracionesmanga.features.profile.domain.User
 import kotlinx.coroutines.Dispatchers
@@ -11,18 +16,54 @@ import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
-class ProfileViewModel(private val getUserLoggedUseCase: GetUserLoggedUseCase) : ViewModel() {
+class ProfileViewModel(
+    private val getUserLoggedUseCase: GetUserLoggedUseCase,
+    private val getAssessmentByUserEmailUseCase: GetAssessmentByUserEmailUseCase,
+    private val getMangasUseCase: GetMangasUseCase,
+    private val getAssessmentMangaUseCase: GetAssessmentMangaUseCase
+) : ViewModel() {
 
     private val _uiState = MutableLiveData(UiState())
     val uiState: LiveData<UiState> = _uiState
 
     fun getUserLogged() {
+        _uiState.postValue(UiState(isLoading = true))
         viewModelScope.launch(Dispatchers.IO) {
             _uiState.postValue(UiState(getUserLoggedUseCase.invoke()))
         }
     }
 
+    fun getAssessment(email: String) {
+        _uiState.postValue(UiState(isLoading = true))
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = getAssessmentByUserEmailUseCase.invoke(email)
+            val resultMangas = getMangasUseCase.invoke()
+
+            val assessments = result.getOrNull()
+            val mangas = resultMangas.getOrNull()
+
+            assessments?.let {
+                mangas?.let {
+                    _uiState.postValue(UiState(
+                        assessment = getAssessmentMangaUseCase.invoke(assessments, mangas)
+                    ))
+                } ?: run {
+                    _uiState.postValue(UiState(
+                        errorApp = resultMangas.exceptionOrNull() as? ErrorApp
+                    ))
+                }
+            } ?: run {
+                _uiState.postValue(UiState(
+                    errorApp = result.exceptionOrNull() as? ErrorApp
+                ))
+            }
+        }
+    }
+
     data class UiState(
-        val user: User? = null
+        val user: User? = null,
+        val isLoading: Boolean = false,
+        val assessment : List<AssessmentManga>? = null,
+        val errorApp: ErrorApp? = null
     )
 }
